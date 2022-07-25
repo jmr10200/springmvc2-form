f343424500ff74d7b89010a47c881c1c1ea061c9
jmr <jmr10200@gmail.com>
2022-07-25 22:30:44 +0900

    멀티 체크박스

--- src/main/java/hello/itemservice/web/form/FormItemController.java
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -9,7 +9,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/form/items")
@@ -18,6 +20,25 @@ import java.util.List;
 public class FormItemController {
 
     private final ItemRepository itemRepository;
+
+    /**
+     * region 을 modelAttribute 에 추가
+     */
+    @ModelAttribute("regions")
+    public Map<String, String> regions() {
+        Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL", "서울");
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+        return regions;
+
+        // @ModelAttribute 의 특별한 사용법
+        // 등록 폼, 상세화면, 수정 폼에서 모두 서울, 부산, 제주라는 체크 박스를 반복해서 보여주어야 한다.
+        // 이는 각각의 컨트롤러에서 model.addAttribute(...) 로 체크 박스를 구성하는 데이터를 반복해서 넣어줘야 한다.
+        // @ModelAttribute 는 이렇게 컨트롤러에 있는 별도의 메소드에 적용할 수 있다.
+        // 이는 해당 컨트롤러를 요청할 때 regions 에서 반환한 값이 자동으로 모델(model)에 담기게 된다.
+        // 각각의 컨트롤러 메소드에서 model 에 직접 담아서 처리해도 된다.
+    }
 
     @GetMapping
     public String items(Model model) {
@@ -45,6 +66,7 @@ public class FormItemController {
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
         log.info("item.open={}", item.getOpen());
+        log.info("item.regions={}", item.getRegions());
         return "redirect:/form/items/{itemId}";
     }
 
