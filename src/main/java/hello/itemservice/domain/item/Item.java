07f2709ae01e65aef9b0189bc3105a9c31cfd4b1
jmr <jmr10200@gmail.com>
2022-07-25 19:18:47 +0900

    단일 체크박스

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,6 +2,8 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class Item {
 
@@ -9,6 +11,15 @@ public class Item {
     private String itemName;
     private Integer price;
     private Integer quantity;
+
+    // 판매 여부
+    private Boolean open;
+    // 등록 지역
+    private List<String> regions;
+    // 상품 종류
+    private ItemType itemType;
+    // 배송 방식
+    private String deliveryCode;
 
     public Item() {
     }
