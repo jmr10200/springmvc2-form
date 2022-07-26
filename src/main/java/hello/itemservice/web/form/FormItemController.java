426370b178b17533de19bc846be36f9d18a05948
jmr <jmr10200@gmail.com>
2022-07-26 12:38:12 +0900

    셀렉트 박스 (자바 객체 이용)

--- src/main/java/hello/itemservice/web/form/FormItemController.java
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -1,5 +1,6 @@
 package hello.itemservice.web.form;
 
+import hello.itemservice.domain.item.DeliveryCode;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import hello.itemservice.domain.item.ItemType;
@@ -10,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -41,11 +43,30 @@ public class FormItemController {
         // 각각의 컨트롤러 메소드에서 model 에 직접 담아서 처리해도 된다.
     }
 
+    /**
+     * ENUM 타입 이용
+     */
     @ModelAttribute("itemTypes")
     public ItemType[] itemTypes() {
         // itemTypes 를 등록 , 조회, 수정 에서 모두 사용하므로 @ModelAttribute 이용
         // ItemType.values() 를 사용하면 해당 ENUM 의 모든 정보를 배열로 반환한다. 예) [BOOK, FOOD, ETC]
         return ItemType.values();
+    }
+
+    /**
+     * 자바 객체 이용
+     */
+    @ModelAttribute("deliveryCodes")
+    public List<DeliveryCode> deliveryCodes() {
+        // DeliveryCode 를 등록, 조회, 수정 에서 모두 사용하므로 @ModelAttribute 이용
+        // DeliveryCode 라는 자바 객체를 이용
+        List<DeliveryCode> deliveryCodes = new ArrayList<>();
+        deliveryCodes.add(new DeliveryCode("FAST", "빠른배송"));
+        deliveryCodes.add(new DeliveryCode("NORMAL", "일반 배송"));
+        deliveryCodes.add(new DeliveryCode("SLOW", "느린 배송"));
+        // 이 메소드는 컨트롤러가 호출 될 때 마다 사용되므로 deliveryCodes 객체도 계속 생성된다.
+        // 따라서 미리 생성해두고 재사용하는 것이 더 효율적이다. 리팩토링 검토 필요한 부분
+        return deliveryCodes;
     }
 
     @GetMapping
